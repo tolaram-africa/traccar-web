03e1a343582f799470f555a913272b6fa690a85c
radioActive DROID <me@godwin.dev>
2021-08-05 02:10:40 +0100

    Added side fill to shared edit toolbar

--- web/app/view/edit/Toolbar.js
+++ web/app/view/edit/Toolbar.js
@@ -21,29 +21,32 @@ Ext.define('Traccar.view.edit.Toolbar', {
 
     initComponent: function () {
         this.callParent(arguments);
-        this.add(0, [{
-            xtype: 'button',
-            handler: 'onAddClick',
-            reference: 'toolbarAddButton',
-            glyph: 'xf067@FontAwesome',
-            tooltip: Strings.sharedAdd,
-            tooltipType: 'title'
-        }, {
-            xtype: 'button',
-            disabled: true,
-            handler: 'onEditClick',
-            reference: 'toolbarEditButton',
-            glyph: 'xf040@FontAwesome',
-            tooltip: Strings.sharedEdit,
-            tooltipType: 'title'
-        }, {
-            xtype: 'button',
-            disabled: true,
-            handler: 'onRemoveClick',
-            reference: 'toolbarRemoveButton',
-            glyph: 'xf00d@FontAwesome',
-            tooltip: Strings.sharedRemove,
-            tooltipType: 'title'
-        }]);
+        this.add(0, ['-',
+            {
+                xtype: 'button',
+                handler: 'onAddClick',
+                reference: 'toolbarAddButton',
+                glyph: 'xf067@FontAwesome',
+                tooltip: Strings.sharedAdd,
+                tooltipType: 'title'
+            }, {
+                xtype: 'button',
+                disabled: true,
+                handler: 'onEditClick',
+                reference: 'toolbarEditButton',
+                glyph: 'xf040@FontAwesome',
+                tooltip: Strings.sharedEdit,
+                tooltipType: 'title'
+            }, {
+                xtype: 'button',
+                disabled: true,
+                handler: 'onRemoveClick',
+                reference: 'toolbarRemoveButton',
+                glyph: 'xf00d@FontAwesome',
+                tooltip: Strings.sharedRemove,
+                tooltipType: 'title'
+            },
+            '-'
+        ]);
     }
 });
